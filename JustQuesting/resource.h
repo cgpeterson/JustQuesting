26bbb2b12807d480eb04dca786c06971a2a7176e
Cody Peterson <CGPeterson@student.fullsail.edu>
2021-03-15 07:28:38 -0600

    Added Dialog box with title, start, and credits.

---
+++ JustQuesting/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource0.rc
+//
+#define ID_Start                        101
+#define IDC_Title                       1001
+#define IDC_BUTTONSTART                 1002
+#define IDC_BUTTONSTART2                1003
+#define IDC_BUTTONCREDIT                1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
